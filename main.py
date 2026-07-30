e250662855823f6aac737acf0bf95807083ce789
devansh13-tech <devanshchaudhari13@gmail.com>
2026-07-31 01:17:00 +0530

    Completed Jarvis Day 5 greeting system

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
 import pyttsx3
+import datetime
 
-engine = pyttsx3.init()
 
-print("Everything is working!")
+def speak(text):
+    engine = pyttsx3.init()
+    print(f"Speaking: {text}")
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
+
+def wish():
+    hour = datetime.datetime.now().hour
+
+    if hour < 5:
+        speak("Good Night.")
+
+    elif hour < 12:
+        speak("Good Morning.")
+
+    elif hour < 13:
+        speak("Good Noon.")
+
+    elif hour < 18:
+        speak("Good Afternoon.")
+
+    elif hour < 22:
+        speak("Good Evening.")
+
+    else:
+        speak("Good Night.")
+
+
+def start_Jarvis():
+
+    speak("Starting Jarvis.")
+
+    wish()
+
+    speak("I am Jarvis.")
+
+    speak("How can I help you?")
+
+    speak("I am ready.")
+
+
+start_Jarvis()
